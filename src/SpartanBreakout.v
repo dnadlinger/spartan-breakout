981d6a5415545c815d99cbf22b52822def697bf3
David Nadlinger <code@klickverbot.at>
2013-05-23 15:30:37 +0200

    Switched clock source to 40MHz crystal oscillator.
    Audio frequencies obviously changed.

--- src/SpartanBreakout.v
+++ src/SpartanBreakout.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 module SpartanBreakout(
-   input CLK_50M,
+   input CLK_40M,
    output AUDIO_OUT
    );
 
-   AudioPlayer player(.CLK(CLK_50M), .AUDIO(AUDIO_OUT));
+   AudioPlayer player(.CLK(CLK_40M), .AUDIO(AUDIO_OUT));
 endmodule
