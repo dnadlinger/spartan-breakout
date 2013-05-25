8f7a68a8fd06523bb51d033e76947e0660c2f28f
David Nadlinger <code@klickverbot.at>
2013-05-25 23:36:33 +0200

    Switched to 720Hz game logic.
    Allows us to never have objects move faster than one pixel a time step.

--- src/GameLogic.v
+++ src/GameLogic.v
@@ -3,10 +3,10 @@
 /// The main game logic updating the game state once per frame.
 ///
 /// To be able to use a simple collision detection approach, the logic
-/// internally runs in three steps, i.e. effectively at 180 Hz.
+/// internally runs in 12 steps, i.e. effectively at 720 Hz.
 ///
-/// The ball position is internally handled with 3 bits of extra subpixel
-/// position to be able to handle lower velocities smoothly.
+/// All positions and velocities are internally handled in units of pixels with
+/// 6 bits of extra subpixel precision (unless noted otherwise).
 ///
 /// All position reference points (as well as the coordinate origin itself) are
 /// in the top left corner of the respective object.
@@ -29,17 +29,17 @@ module GameLogic(
       BLOCK_STATE <= 72'b111111111111010101010101101010101010010101010101101010101010111111111111;
    end
 
-   // After we got the okay, run the update logic for three clock cycles.
+   // After we got the okay, run the update logic for 12 clock cycles.
    reg doUpdate = 1'b0;
-   reg [1:0] updateCounter = 2'h0;
+   reg [3:0] updateCounter = 4'd0;
 
    always @(posedge CLK) begin
       if (doUpdate) begin
-         if (updateCounter == 2'h2) begin
+         if (updateCounter == 4'd11) begin
             doUpdate <= 1'b0;
-            updateCounter <= 2'h0;
+            updateCounter <= 4'd0;
          end else begin
-            updateCounter <= updateCounter + 2'h1;
+            updateCounter <= updateCounter + 4'd1;
          end
       end else if (START_UPDATE) begin
          doUpdate <= 1'b1;
@@ -74,16 +74,16 @@ module GameLogic(
    parameter Ball_inGame = 2'h1;
    parameter Ball_lost = 3'h2;
    reg [1:0] ballState = Ball_waitForRelease;
-   reg [12:0] ballXSubpixel = {10'd395, 3'h0};
-   reg [12:0] ballYSubpixel = {10'd400, 3'h0};
-   reg [12:0] ballVelocityXSubpixel = 12'h0;
-   reg [12:0] ballVelocityYSubpixel = 12'h0;
+   reg [15:0] ballXSubpixel = {10'd395, 6'h0};
+   reg [15:0] ballYSubpixel = {10'd400, 6'h0};
+   reg [15:0] ballVelocityXSubpixel = 15'h0;
+   reg [15:0] ballVelocityYSubpixel = 15'h0;
 
-   wire [12:0] tentativeBallXSubpixel = ballXSubpixel + ballVelocityXSubpixel;
-   wire [9:0] tentativeBallXPixel = tentativeBallXSubpixel[12:3];
-   wire [6:0] tentativeBallXTile = tentativeBallXSubpixel[12:6];
-   wire [12:0] tentativeBallYSubpixel = ballYSubpixel + ballVelocityYSubpixel;
-   wire [6:0] tentativeBallYTile = tentativeBallYSubpixel[12:6];
+   wire [15:0] tentativeBallXSubpixel = ballXSubpixel + ballVelocityXSubpixel;
+   wire [9:0] tentativeBallXPixel = tentativeBallXSubpixel[15:6];
+   wire [6:0] tentativeBallXTile = tentativeBallXSubpixel[15:9];
+   wire [15:0] tentativeBallYSubpixel = ballYSubpixel + ballVelocityYSubpixel;
+   wire [6:0] tentativeBallYTile = tentativeBallYSubpixel[15:9];
    wire bounceLeft = tentativeBallXTile == leftWallXTile;
    wire bounceRight = tentativeBallXTile == rightWallXTile - 7'd1;
    wire bounceTop = tentativeBallYTile == ceilingYTile;
@@ -98,11 +98,11 @@ module GameLogic(
                if (BTN_RELEASE) begin
                   ballState <= Ball_inGame;
                   // TODO: Generate velocity based on frame counter.
-                  ballVelocityXSubpixel <= {9'd0, 3'd1};
-                  ballVelocityYSubpixel <= -{9'd1, 3'd0};
+                  ballVelocityXSubpixel <= {10'd0, 6'd1};
+                  ballVelocityYSubpixel <= -{10'd0, 6'd4};
                end
-               ballXSubpixel <= {PADDLE_X_PIXEL + ((paddleLengthPixel - ballSizePixel) / 2), 3'd0};
-               ballYSubpixel <= {(paddleYPixel - ballSizePixel), 3'd0};
+               ballXSubpixel <= {PADDLE_X_PIXEL + ((paddleLengthPixel - ballSizePixel) / 2), 6'd0};
+               ballYSubpixel <= {(paddleYPixel - ballSizePixel), 6'd0};
             end
             Ball_inGame: begin
                if (!(bounceLeft && bounceRight)) begin
@@ -130,6 +130,6 @@ module GameLogic(
       end
    end
 
-   assign BALL_X_PIXEL = ballXSubpixel[12:3];
-   assign BALL_Y_PIXEL = ballYSubpixel[12:3];
+   assign BALL_X_PIXEL = ballXSubpixel[15:6];
+   assign BALL_Y_PIXEL = ballYSubpixel[15:6];
 endmodule
