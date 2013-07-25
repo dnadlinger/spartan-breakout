168dc016ffcb59f23312240b486187692b81da3d
David Nadlinger <code@klickverbot.at>
2013-07-26 00:04:02 +0200

    Updated top-level testbench for new buttons/switches.

--- src/test/SpartanBreakoutTest.v
+++ src/test/SpartanBreakoutTest.v
@@ -5,6 +5,11 @@ module SpartanBreakoutTest;
    reg CLK_40M;
    reg BTN_LEFT;
    reg BTN_RIGHT;
+   reg BTN_A;
+   reg BTN_B;
+   reg SW_RESET;
+   reg SW_PAUSE;
+   reg SW_IGNORE_DEATH;
 
    // Outputs
    wire [7:0] COLOR;
@@ -17,6 +22,11 @@ module SpartanBreakoutTest;
       .CLK_40M(CLK_40M),
       .BTN_LEFT(BTN_LEFT),
       .BTN_RIGHT(BTN_RIGHT),
+      .BTN_A(BTN_A),
+      .BTN_B(BTN_B),
+      .SW_RESET(SW_RESET),
+      .SW_PAUSE(SW_PAUSE),
+      .SW_IGNORE_DEATH(SW_IGNORE_DEATH),
       .COLOR(COLOR),
       .HSYNC(HSYNC),
       .VSYNC(VSYNC),
@@ -27,6 +37,14 @@ module SpartanBreakoutTest;
       // Initialize Inputs
       CLK_40M = 0;
 
+      BTN_LEFT <= 0;
+      BTN_RIGHT <= 0;
+      BTN_A <= 0;
+      BTN_B <= 0;
+      SW_RESET <= 0;
+      SW_PAUSE <= 0;
+      SW_IGNORE_DEATH <= 0;
+
       // Wait 100 ns for global reset to finish
       #100;
 
@@ -35,12 +53,5 @@ module SpartanBreakoutTest;
          #12.5;
          CLK_40M = ~CLK_40M;
       end
-   end
-
-   initial begin
-      BTN_LEFT = 1;
-      BTN_RIGHT = 0;
-      #30000000
-      BTN_LEFT = 0;
    end
 endmodule
